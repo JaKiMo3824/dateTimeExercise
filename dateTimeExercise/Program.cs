ef07c4de61e5abfbf731d7d4e3e7872e553a33b7
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-29 13:47:50 -0700

    dateTimeExercise
    exercises 1-3

--- dateTimeExercise/Program.cs
+++ dateTimeExercise/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace dateTimeExercise
 {
@@ -10,6 +11,14 @@ namespace dateTimeExercise
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("The current date and time is: " + DateTime.Now);
+            Console.WriteLine("Please enter a number to represent hours.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            DateTime current = DateTime.Now;
+            Console.WriteLine("The current date and time plus the number of hours you entered equals: {0} ", current.AddHours(userInput));
+            Console.ReadLine();
+
+
         }
     }
 }
